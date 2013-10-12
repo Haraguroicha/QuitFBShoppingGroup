60b31fffba4ffdc7747822fdb78c08f40b189151
腹黒い茶 <haraguroicha@hhmr.biz>
2013-10-12 10:27:41 +0800

    initial pages commit

--- report.js
+++
@@ -1,6 +0,0 @@
-var _rg = '';
-if(location.hash.length > 0)
-	_rg = location.hash.substr(1).replace(/_rg=/g, '');
-if(_rg.length > 0) {
-	document.body.scrollTop = $('#group').val(_rg).parents('div.well')[0].offsetTop - 10;
-}
